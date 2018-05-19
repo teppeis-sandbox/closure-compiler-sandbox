07ac162936a248997fd51c17c924ed2a1b8711d7
teppeis <teppeis@gmail.com>
2018-05-19 11:50:21 +0900

    tmp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-alert(Symbol());
-alert('iterator' in Symbol);
-alert([][Symbol.iterator]());
-alert(typeof Number.isInteger);
-alert(typeof Number.EPSILON);
-alert(typeof String.prototype.repeat === "function" && "a".repeat(3));
+/**
+ * @suppress {uselessCode}
+ */
+Map;
+Set;
